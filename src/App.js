d3b23973d949a8b9858fad6d8be7efea8ddcd0b4
Your Name <you@example.com>
2023-07-24 20:49:04 -0600

    Instalacion de TypeScript

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'tailwindcss/tailwind.css';
-import './App.css';
-import Layout from './componentes/compartidos/Layout';
-import Lista from './componentes/lista/Lista';
-import Detalles from './componentes/nueva/Detalles';
-import NoEncontrado from './componentes/compartidos/NoEncontrado';
+import { Route, Routes } from "react-router-dom";
+import "tailwindcss/tailwind.css";
+import "./App.css";
+import Layout from "./componentes/compartidos/Layout";
+import Lista from "./componentes/lista/Lista";
+import Detalles from "./componentes/nueva/Detalles";
+import NoEncontrado from "./componentes/compartidos/NoEncontrado";
+import Modal from "./componentes/compartidos/Modal";
 
 function App() {
-  return (
-    
-      <Routes> 
-        <Route path="/" element={<Layout />} >
-        <Route index element={<Lista />} />
-        <Route path="/lista" element={<Lista />} />
-        <Route path="/nueva" element={<Detalles />} />
-        </Route>
-        <Route path="*" element={<NoEncontrado />} />
-        
-      </Routes>
-   
-  );
+    //const  [, despachar] = useContext(Contexto);
+
+    //useEffect(() => {
+    //const metas = pedirMetas();
+    //enviar({ tipo: 'colocar', metas });
+
+    //}, [enviar]);
+
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Lista />} />
+                <Route path="lista" element={<Lista />}>
+                    <Route
+                        path=":id"
+                        element={
+                            <Modal>
+                                <Detalles />
+                            </Modal>
+                        }
+                    />
+                </Route>
+                <Route path="nueva" element={<Detalles />} />
+            </Route>
+            <Route path="*" element={<NoEncontrado />} />
+        </Routes>
+    );
 }
 
 export default App;
-
